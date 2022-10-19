ff03a3771db8b1b44135a21bf8de232945a47679
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2022-10-19 02:50:19 +0000

    Deploying to gh-pages from @ vineethsagar/tools@59b1826944d7480357d53f8de738a061f661384f 🚀

---
+++ index.js
@@ -0,0 +1,222 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf, __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: !0 });
+}, __copyProps = (to, from, except, desc) => {
+  if (from && typeof from == "object" || typeof from == "function")
+    for (let key of __getOwnPropNames(from))
+      !__hasOwnProp.call(to, key) && key !== except && __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: !0 }) : target,
+  mod
+)), __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: !0 }), mod);
+
+// <stdin>
+var stdin_exports = {};
+__export(stdin_exports, {
+  assets: () => assets_manifest_default,
+  assetsBuildDirectory: () => assetsBuildDirectory,
+  entry: () => entry,
+  publicPath: () => publicPath,
+  routes: () => routes
+});
+module.exports = __toCommonJS(stdin_exports);
+
+// app/entry.server.tsx
+var entry_server_exports = {};
+__export(entry_server_exports, {
+  default: () => handleRequest
+});
+var import_stream = require("stream"), import_node = require("@remix-run/node"), import_react = require("@remix-run/react"), import_isbot = __toESM(require("isbot")), import_server = require("react-dom/server"), import_jsx_runtime = require("react/jsx-runtime"), ABORT_DELAY = 5e3;
+function handleRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  return (0, import_isbot.default)(request.headers.get("user-agent")) ? handleBotRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext
+  ) : handleBrowserRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext
+  );
+}
+function handleBotRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  return new Promise((resolve, reject) => {
+    let didError = !1, { pipe, abort } = (0, import_server.renderToPipeableStream)(
+      /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react.RemixServer, {
+        context: remixContext,
+        url: request.url
+      }),
+      {
+        onAllReady() {
+          let body = new import_stream.PassThrough();
+          responseHeaders.set("Content-Type", "text/html"), resolve(
+            new import_node.Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode
+            })
+          ), pipe(body);
+        },
+        onShellError(error) {
+          reject(error);
+        },
+        onError(error) {
+          didError = !0, console.error(error);
+        }
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+function handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  return new Promise((resolve, reject) => {
+    let didError = !1, { pipe, abort } = (0, import_server.renderToPipeableStream)(
+      /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react.RemixServer, {
+        context: remixContext,
+        url: request.url
+      }),
+      {
+        onShellReady() {
+          let body = new import_stream.PassThrough();
+          responseHeaders.set("Content-Type", "text/html"), resolve(
+            new import_node.Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode
+            })
+          ), pipe(body);
+        },
+        onShellError(err) {
+          reject(err);
+        },
+        onError(error) {
+          didError = !0, console.error(error);
+        }
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+
+// app/root.tsx
+var root_exports = {};
+__export(root_exports, {
+  default: () => App,
+  meta: () => meta
+});
+var import_react2 = require("@remix-run/react"), import_jsx_runtime = require("react/jsx-runtime"), meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1"
+});
+function App() {
+  return /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("html", {
+    lang: "en",
+    children: [
+      /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("head", {
+        children: [
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react2.Meta, {}),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react2.Links, {})
+        ]
+      }),
+      /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("body", {
+        children: [
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react2.Outlet, {}),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react2.ScrollRestoration, {}),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react2.Scripts, {}),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)(import_react2.LiveReload, {})
+        ]
+      })
+    ]
+  });
+}
+
+// app/routes/index.tsx
+var routes_exports = {};
+__export(routes_exports, {
+  default: () => Index
+});
+var import_jsx_runtime = require("react/jsx-runtime");
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("div", {
+    style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.4" },
+    children: [
+      /* @__PURE__ */ (0, import_jsx_runtime.jsx)("h1", {
+        children: "Welcome to Remix"
+      }),
+      /* @__PURE__ */ (0, import_jsx_runtime.jsxs)("ul", {
+        children: [
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)("a", {
+              target: "_blank",
+              href: "https://remix.run/tutorials/blog",
+              rel: "noreferrer",
+              children: "15m Quickstart Blog Tutorial"
+            })
+          }),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)("a", {
+              target: "_blank",
+              href: "https://remix.run/tutorials/jokes",
+              rel: "noreferrer",
+              children: "Deep Dive Jokes App Tutorial"
+            })
+          }),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)("a", {
+              target: "_blank",
+              href: "https://remix.run/docs",
+              rel: "noreferrer",
+              children: "Remix Docs"
+            })
+          }),
+          /* @__PURE__ */ (0, import_jsx_runtime.jsx)("li", {
+            children: /* @__PURE__ */ (0, import_jsx_runtime.jsx)("a", {
+              target: "_blank",
+              href: "https://remix.run/docs",
+              rel: "noreferrer",
+              children: "This is a personal tools library"
+            })
+          })
+        ]
+      })
+    ]
+  });
+}
+
+// server-assets-manifest:@remix-run/dev/assets-manifest
+var assets_manifest_default = { version: "d118548a", entry: { module: "/build/entry.client-SJY4QB6I.js", imports: ["/build/_shared/chunk-7S4CMYTF.js", "/build/_shared/chunk-NNE7662T.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-YHQGQIYW.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-6GSYKZW3.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, url: "/build/manifest-D118548A.js" };
+
+// server-entry-module:@remix-run/dev/server-build
+var assetsBuildDirectory = "public/build", publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
+  root: {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: root_exports
+  },
+  "routes/index": {
+    id: "routes/index",
+    parentId: "root",
+    path: void 0,
+    index: !0,
+    caseSensitive: void 0,
+    module: routes_exports
+  }
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  assets,
+  assetsBuildDirectory,
+  entry,
+  publicPath,
+  routes
+});
